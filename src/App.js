ee5e026aded878a5f6ec2521746f7e26a5cdc438
JulisaDiego <julisadiego@yahoo.com>
2020-01-15 15:37:56 -0500

    MVP met

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+
+    <Route exact path ='/'></Route>
+    <Route exact path ='/about'></Route>
+    <Route exact path ='/contact'></Route>
+    
   </div>
 );
 
